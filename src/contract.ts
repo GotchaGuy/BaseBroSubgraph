716ec38134c4525a976408d150ca21bbd55bc677
Aljosa Makevic <aljosa100@gmail.com>
2023-01-24 07:12:40 +0100

    base-nft-release

--- src/contract.ts
+++ src/contract.ts
@@ -1,148 +1,156 @@
-import {
-  AirdropFunded as AirdropFundedEvent,
-  ApprovalForAll as ApprovalForAllEvent,
-  Claimed as ClaimedEvent,
-  ContractURISet as ContractURISetEvent,
-  Initialized as InitializedEvent,
-  MerkleRootSet as MerkleRootSetEvent,
-  TransferBatch as TransferBatchEvent,
-  TransferSingle as TransferSingleEvent,
-  URI as URIEvent
-} from "../generated/Contract/Contract"
-import {
-  AirdropFunded,
-  ApprovalForAll,
-  Claimed,
-  ContractURISet,
-  Initialized,
-  MerkleRootSet,
-  TransferBatch,
-  TransferSingle,
-  URI
-} from "../generated/schema"
+// import {
+//   AirdropFunded as AirdropFundedEvent,
+//   ApprovalForAll as ApprovalForAllEvent,
+//   Claimed as ClaimedEvent,
+//   ContractURISet as ContractURISetEvent,
+//   Initialized as InitializedEvent,
+//   MerkleRootSet as MerkleRootSetEvent,
+//   TransferBatch as TransferBatchEvent,
+//   TransferSingle as TransferSingleEvent,
+//   URI as URIEvent
+// } from "../generated/Contract/Contract"
+import { Claimed } from "../generated/Contract/Contract"
+import { Claimer } from "../generated/schema"
 
-export function handleAirdropFunded(event: AirdropFundedEvent): void {
-  let entity = new AirdropFunded(
+
+// export function handleAirdropFunded(event: AirdropFundedEvent): void {
+//   let entity = new AirdropFunded(
+//     event.transaction.hash.concatI32(event.logIndex.toI32())
+//   )
+//   entity.contractAddress = event.params.contractAddress
+
+//   entity.blockNumber = event.block.number
+//   entity.blockTimestamp = event.block.timestamp
+//   entity.transactionHash = event.transaction.hash
+
+//   entity.save()
+// }
+
+// export function handleApprovalForAll(event: ApprovalForAllEvent): void {
+//   let entity = new ApprovalForAll(
+//     event.transaction.hash.concatI32(event.logIndex.toI32())
+//   )
+//   entity.account = event.params.account
+//   entity.operator = event.params.operator
+//   entity.approved = event.params.approved
+
+//   entity.blockNumber = event.block.number
+//   entity.blockTimestamp = event.block.timestamp
+//   entity.transactionHash = event.transaction.hash
+
+//   entity.save()
+// }
+
+
+export function handleClaimed(event: Claimed): void {
+  let claimer = new Claimer(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  entity.contractAddress = event.params.contractAddress
+  
+  claimer.id = event.params.claimer;
+  claimer.timestamp = event.block.timestamp;
+  
+  // claimer.timestamp = (Math.floor(Date.now() / 1000)).toString();
+  // entity.blockNumber = event.block.number
+  // entity.blockTimestamp = event.block.timestamp
+  // entity.transactionHash = event.transaction.hash
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  claimer.save()
 }
+// export function handleClaimed(event: ClaimedEvent): void {
+//   let entity = new Claimed(
+//     event.transaction.hash.concatI32(event.logIndex.toI32())
+//   )
+//   entity.claimer = event.params.claimer
 
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.account = event.params.account
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
+//   entity.blockNumber = event.block.number
+//   entity.blockTimestamp = event.block.timestamp
+//   entity.transactionHash = event.transaction.hash
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+//   entity.save()
+// }
 
-  entity.save()
-}
+// export function handleContractURISet(event: ContractURISetEvent): void {
+//   let entity = new ContractURISet(
+//     event.transaction.hash.concatI32(event.logIndex.toI32())
+//   )
+//   entity.contractURI = event.params.contractURI
 
-export function handleClaimed(event: ClaimedEvent): void {
-  let entity = new Claimed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.claimer = event.params.claimer
+//   entity.blockNumber = event.block.number
+//   entity.blockTimestamp = event.block.timestamp
+//   entity.transactionHash = event.transaction.hash
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+//   entity.save()
+// }
 
-  entity.save()
-}
+// export function handleInitialized(event: InitializedEvent): void {
+//   let entity = new Initialized(
+//     event.transaction.hash.concatI32(event.logIndex.toI32())
+//   )
+//   entity.version = event.params.version
 
-export function handleContractURISet(event: ContractURISetEvent): void {
-  let entity = new ContractURISet(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.contractURI = event.params.contractURI
+//   entity.blockNumber = event.block.number
+//   entity.blockTimestamp = event.block.timestamp
+//   entity.transactionHash = event.transaction.hash
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+//   entity.save()
+// }
 
-  entity.save()
-}
+// export function handleMerkleRootSet(event: MerkleRootSetEvent): void {
+//   let entity = new MerkleRootSet(
+//     event.transaction.hash.concatI32(event.logIndex.toI32())
+//   )
+//   entity.merkleRoot = event.params.merkleRoot
 
-export function handleInitialized(event: InitializedEvent): void {
-  let entity = new Initialized(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.version = event.params.version
+//   entity.blockNumber = event.block.number
+//   entity.blockTimestamp = event.block.timestamp
+//   entity.transactionHash = event.transaction.hash
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+//   entity.save()
+// }
 
-  entity.save()
-}
+// export function handleTransferBatch(event: TransferBatchEvent): void {
+//   let entity = new TransferBatch(
+//     event.transaction.hash.concatI32(event.logIndex.toI32())
+//   )
+//   entity.operator = event.params.operator
+//   entity.from = event.params.from
+//   entity.to = event.params.to
+//   entity.ids = event.params.ids
+//   entity.values = event.params.values
 
-export function handleMerkleRootSet(event: MerkleRootSetEvent): void {
-  let entity = new MerkleRootSet(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.merkleRoot = event.params.merkleRoot
+//   entity.blockNumber = event.block.number
+//   entity.blockTimestamp = event.block.timestamp
+//   entity.transactionHash = event.transaction.hash
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+//   entity.save()
+// }
 
-  entity.save()
-}
+// export function handleTransferSingle(event: TransferSingleEvent): void {
+//   let entity = new TransferSingle(
+//     event.transaction.hash.concatI32(event.logIndex.toI32())
+//   )
+//   entity.operator = event.params.operator
+//   entity.from = event.params.from
+//   entity.to = event.params.to
+//   entity.id = event.params.id
+//   entity.value = event.params.value
 
-export function handleTransferBatch(event: TransferBatchEvent): void {
-  let entity = new TransferBatch(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.operator = event.params.operator
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.ids = event.params.ids
-  entity.values = event.params.values
+//   entity.blockNumber = event.block.number
+//   entity.blockTimestamp = event.block.timestamp
+//   entity.transactionHash = event.transaction.hash
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+//   entity.save()
+// }
 
-  entity.save()
-}
+// export function handleURI(event: URIEvent): void {
+//   let entity = new URI(event.transaction.hash.concatI32(event.logIndex.toI32()))
+//   entity.value = event.params.value
+//   entity.id = event.params.id
 
-export function handleTransferSingle(event: TransferSingleEvent): void {
-  let entity = new TransferSingle(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.operator = event.params.operator
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.id = event.params.id
-  entity.value = event.params.value
+//   entity.blockNumber = event.block.number
+//   entity.blockTimestamp = event.block.timestamp
+//   entity.transactionHash = event.transaction.hash
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleURI(event: URIEvent): void {
-  let entity = new URI(event.transaction.hash.concatI32(event.logIndex.toI32()))
-  entity.value = event.params.value
-  entity.id = event.params.id
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+//   entity.save()
+// }
